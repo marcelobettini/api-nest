b49f6982487c5827626447db98ac3b44fcadcc87
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2024-03-15 21:31:36 -0300

    servicio getTrackById + inyeccion de dependencias con @Parama()

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { iTrack } from './track.interface';
-const base_url: string = 'http://localhost:3030/tracks';
+const base_url: string = 'http://localhost:3030/tracks/';
 @Injectable()
 export class TrackService {
   async getTracks(): Promise<iTrack[]> {
     const res = await fetch(base_url);
     const tracks = await res.json();
     return tracks;
+  }
+  async getTrackById(id: number): Promise<iTrack> {
+    console.log(id);
+    const res = await fetch(base_url + id);
+    const track = await res.json();
+    return track;
   }
 }
